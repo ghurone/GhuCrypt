6f2acd21371156027c5ef20d667d8b1a5c4af2c6
Ghuron <ghuron@usp.br>
2021-12-10 20:58:36 -0300

    Add seed

--- zequinha.py
+++ zequinha.py
@@ -1,28 +1,38 @@
+from random import Random
+
 class Zequinha:
-    def __init__(self) -> None:
-        self.base = 'ZEQUINHAzequinha'
+
+    def __init__(self, seed = None) -> None:
+        self.base = list('ZEQUINHAzequinha')  # Chave base
+        
+        # Caso insira uma seed ele embaralha a chave base em relação a essa seed.
+        if seed != None:
+            Random(seed).shuffle(self.base)
 
     def encriptar(self, string:str) -> str:
-        str_bin = ''  # String que recebe os valores, em binário, de cada caractere do texto
-        string_list = []  # Lista que recebe os valores em decimal de cada binário 4b
-        str_crypted = ''  # String com o texto encriptado
+        if isinstance(string, str):
+            str_bin = ''  # String que recebe os valores, em binário, de cada caractere do texto
+            string_list = []  # Lista que recebe os valores em decimal de cada binário 4b
+            str_crypted = ''  # String com o texto encriptado
 
-        for i in range(0, len(string)):
-            dec = ord(string[i])  # Recebe o valor do caractere em decimal
-            str_bin += f'{dec:08b}'  # Transforma esse decimal em um binário de 8b e os concatena
+            for i in range(0, len(string)):
+                dec = ord(string[i])  # Recebe o valor do caractere em decimal
+                str_bin += f'{dec:08b}'  # Transforma esse decimal em um binário de 8b e os concatena
 
-        # Divide os binários concatenados em 4b, e adiciona esse valor em decimal numa lista
-        for i in range(0, len(str_bin), 4):
-            string_list.append(int(str_bin[i:i + 4], 2))
+            # Divide os binários concatenados em 4b, e adiciona esse valor em decimal numa lista
+            for i in range(0, len(str_bin), 4):
+                string_list.append(int(str_bin[i:i + 4], 2))
 
-        # Os valores em decimais são usados como índices da "Tabela", então cada valor representa um elemento
-        for i, v in enumerate(string_list):
-            str_crypted += self.base[v]
+            # Os valores em decimais são usados como índices da "Tabela", então cada valor representa um elemento
+            for i, v in enumerate(string_list):
+                str_crypted += self.base[v]
 
-        return str_crypted
+            return str_crypted
+        
+        raise TypeError("O paramêtro `string` precisa ser do tipo STR")
 
     def desencriptar(self, string:str) -> str:
-        if self.is_zeca(string):
+        if self.__is_zeca(string):
             list_index = []  # Lista que contém os índices de cada caractere em relação a "Tabela"
             string_decr = ''  # String que guarda os binários dos índices concatenados
             str_decrypted = ''  # String que armazena os binários transformados em caracteres (bin -> dec - > char)
@@ -43,11 +53,18 @@ class Zequinha:
         
         raise ValueError('A string inserida não está em Zequinha.')
 
-    def is_zeca(self, string):
-        letras = set(string)  # um set() com todos os chars da string
+    def __is_zeca(self, string: str) -> bool:
+        """
+            Método que verifica se todos os caracteres da string inserida
+        estão contidos na chave base.
+        """
+        if isinstance(string, str):
+            letras = set(string)  # um set() com todos os chars da string
 
-        for letra in letras:
-            if letra not in self.base:
-                return False
+            for letra in letras:
+                if letra not in self.base:
+                    return False
+            
+            return True
         
-        return True
+        raise TypeError("O paramêtro `string` precisa ser do tipo STR")
